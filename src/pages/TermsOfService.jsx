63c06c46bc458492cc7879d0a02c2ede9745b47e
MatthewFoster02 <mattfoster02@outlook.com>
2024-09-25 16:07:39 +0100

    tos

---
+++ src/pages/TermsOfService.jsx
@@ -0,0 +1,201 @@
+import ReactMarkdown from 'react-markdown';
+
+const markdownTermsOfService = `
+# Terms and Conditions
+
+Our iOS app, edio, is designed to provide you with innovative learning tools and resources. By accessing and using edio, you agree to the following terms and conditions, which we ask you to read carefully.
+
+## Services Offered
+
+edio is proud to offer an AI-powered Creator-Subscriber platform tailored for educational purposes, allowing the creation and delivery of creator content as quizzes and games which allow subscribers to learn and test themselves on this content.
+
+## Collection and Use of Personal Information
+
+To unlock the full potential of edio, users are required to register and submit certain personal details, including but not limited to, your name, email address, and profile picture (not required). This information facilitates content customization, enhancing your learning journey, and to allow creators and subscribers to connect seamlessly.
+
+## Data Protection
+
+We prioritise the security of your personal information, storing all user data within the secure confines of a MongoDB Atlas cloud database. This, along with axios, the HTTP request provider, are the sole third-party services with access to your information, underlining our commitment to data privacy and protection.
+
+## Content and User Conduct
+
+edio is a platform built on respect and integrity. Sharing of accounts is strictly forbidden, as are any forms of harassment, cheating, or abuse.
+
+## Intellectual Property
+
+The educational content, including videos and other materials provided by edio, is designed exclusively for the personal and educational use of our registered users.
+
+## Disclaimer and Liability
+
+Although we strive for accuracy, edio cannot guarantee the completeness, reliability, or up-to-dateness of the educational content provided. Therefore, we shall not be held liable for any damages or losses incurred from your use of our app, within the limits of applicable law.
+
+## Amendments to Terms and Conditions
+
+edio reserves the right to modify these terms at any discretion without prior notification. Changes will be communicated via our app and, when appropriate, through direct email. Continued use of our services following such modifications indicates your acceptance of the new terms.
+
+## Governing Law
+
+These Terms and Conditions are governed by Irish law, with any disputes subject to the jurisdiction of Irish courts. Invalid or unenforceable terms will be excised without affecting the remainder.
+- The governing law for these Terms and Conditions is the Republic of Ireland's legislation. Your engagement with edio signifies your agreement to abide by these terms and subjects you to the exclusive jurisdiction of the Irish courts for any disputes arising from these terms.
+- Should any provision of these Terms and Conditions be found invalid or unenforceable, such provision will be severed, yet this will not impact the enforceability of the remaining provisions.
+- edio asserts its right to uphold and enforce these Terms and Conditions at all times. Our failure to exercise or enforce any right or provision shall not constitute a waiver of such right or provision in that or any other instance.
+- We reserve the right to amend or update these Terms and Conditions as necessary, without prior notice. You will be informed of any significant changes through an email notification or an announcement posted prominently on our app.
+
+## Guidelines for Acceptable Use
+
+This section outlines acceptable use policies for our app, applicable to all our users. By utilising our app, you agree to comply with these guidelines as part of our broader Terms and Conditions.
+
+### Only for Lawful Use
+- **Compliance with Laws:** Our app is to be used strictly for lawful purposes. Any use that violates local, national, or international laws or regulations is prohibited.
+- **Restriction on Harmful Content:** Engaging in activities that harm or have the potential to harm minors is forbidden. This includes sending, uploading, downloading, or using materials that fail to meet our content standards.
+- **Prohibition of Spam and Malicious Code:** Our platform must not be used to distribute spam or to transmit harmful code such as viruses, Trojan horses, or other damaging computer code.
+
+### Prohibited Activities
+- **Legal Violations:** Utilising our app in any manner that breaches any applicable laws is not allowed.
+- **Fraudulent Behaviour:** The app may not be employed for fraudulent activities or any purposes that are illegal.
+- **Endangering Minors:** It is against our policy to use the app in ways that could be detrimental to minors.
+- **Disclosure of Sensitive Information:** Publicising private information that could endanger safety or data protection is prohibited.
+- **Non-compliant Content:** Sending, uploading, downloading, or using material that does not align with our content guidelines is forbidden.
+- **Spamming Activities:** Unsolicited advertising or promotional materials are not permitted.
+- **Transmission of Harmful Software:** It is prohibited to disseminate viruses, worms, spyware, adware, or other malicious software.
+- **Non-Interference Clause:** Unauthorised access or attempts to interfere with, damage, or disrupt any aspect of our app, including but not limited to its infrastructure, networks, or software, whether owned by edio or by external parties, is strictly prohibited.
+
+### Further Limitations
+- **Restrictions on Reselling:** Duplicating, copying, or reselling any part of our app contrary to our terms of use is not allowed.
+- **No Disruption:** Interfering with, damaging, or disrupting any part of our app, including its underlying infrastructure, networks, or software, either ours or used by third parties, is strictly prohibited.
+- Following these guidelines is essential to maintain a secure and lawful environment for all edio users, promoting a constructive and positive educational experience. Failure to comply with these policies could lead to measures such as suspension or termination of service access.
+- **Redistribution:** Duplication, copying, or resale of any portion of the app contrary to the stipulated terms of use is strictly forbidden.
+- **Non-Interference Clause:** Unauthorized access or attempts to interfere with, damage, or disrupt any aspect of our app, including but not limited to its infrastructure, networks, or software, whether owned by edio or by external parties, is strictly prohibited.
+
+## Suspension and Termination
+
+At edio, we are committed to maintaining a respectful and secure learning environment. In line with this commitment, we will thoroughly investigate any suspected violations of our Acceptable Use Policy encountered during your interaction with our platform. Should any breach be confirmed, we reserve the right to implement measures we consider appropriate to address the situation. Actions taken in response to significant breaches of our Terms and Conditions may include, but are not limited to, the following:
+- **Temporary or Permanent Suspension:** We may suspend your access to edio, either temporarily or permanently, revoking your ability to use our services.
+- **Removal of Content:** Any contributions or materials you have uploaded to edio may be removed temporarily or permanently.
+- **Issuance of Warnings:** You may receive formal warnings regarding your conduct on the platform.
+- **Legal Proceedings:** We may initiate legal action against you to recover costs associated with the breach or for other damages.
+- **Further Legal Action:** Depending on the severity of the breach, additional legal measures may be pursued as necessary.
+- **Disclosure to Law Enforcement:** If deemed necessary and reasonable, we may disclose pertinent information to law enforcement authorities in response to the breach.
+
+edio assumes no liability for actions taken in response to breaches of the Acceptable Use Policy. This list of responses is not exhaustive, and we reserve the right to undertake other actions as deemed appropriate under the circumstances. Your adherence to our Acceptable Use Policy ensures edio remains a productive, safe, and respectful environment for all users. Violations of these terms compromise the integrity of our learning community and will be addressed accordingly.
+
+## Content Persistence and Removal
+
+While edio typically maintains uploaded content, there are instances where removal is necessary, including but not limited to:
+- **Copyright Claims:** Upon receiving legitimate claims of copyright infringement, the offending material will be removed promptly.
+- **Inappropriate Content:** Any content found violating our established Content Standards will be subject to removal at our discretion.
+- **User Requests:** We diligently address user requests for content removal, aiming for swift resolution.
+
+Please note, removed content may still remain in some form due to backups and search engine caches, which are beyond our immediate control.
+
+## Copyright Notice
+
+edio respects copyright and intellectual property rights, which extend to the textual content and various materials provided as part of our services. We endeavour to obtain appropriate permissions for any content reproduced on our app. If you suspect that your copyright has been infringed upon by any material available through edio, we encourage you to reach out to us at [contact@edio.cc](mailto:contact@edio.cc), providing:
+- A detailed description of the copyrighted work you believe has been infringed.
+- The specific location on edio where you found the alleged infringement.
+- Your contact details for further communication.
+
+We take copyright concerns very seriously and are committed to addressing any issues promptly and effectively.
+
+## Content Guidelines
+
+These guidelines are applicable to all user-generated content on edio, such as comments, reports, and other forms of submissions. To ensure the integrity and originality of content, we reserve the right to review submissions using anti plagiarism tools. All contributions by users should adhere to the following principles:
+- **Accuracy:** Any factual assertions in contributions must be verifiable and accurate.
+- **Authenticity:** Opinions shared should reflect the true views of the contributor.
+- **Legal Compliance:** All contributions must abide by the relevant laws of Ireland and any other country from where they are submitted.
+
+Contributions are prohibited from:
+- Containing sexually explicit content.
+- Encouraging or glorifying violence.
+- Discriminating against individuals based on race, gender, religion, nationality, disability, sexual orientation, or age.
+- Being defamatory, obscene, or likely to incite hatred.
+- Violating intellectual property rights, including copyright, database rights, or trademarks belonging to others.
+- Deceiving or misleading others.
+- Targeting individuals in a way that could harass, distress, embarrass, alarm, or annoy.
+- Impersonating others or falsely representing affiliations.
+- Violating legal duties owed to a third party, such as contractual duties or confidentiality.
+- Promoting or facilitating illegal activities.
+
+Our commitment is to maintain edio as a respectful and law-abiding community. We encourage all users to contribute positively and responsibly.
+
+## Forbidden Activities
+
+- **Violation of Laws:** Utilising our app in manners that contravene any applicable local, national, or international laws or regulations is strictly prohibited.
+- **Engagement in Fraud:** The app must not be used for engaging in or facilitating fraudulent or illegal activities.
+- **Endangering Youth:** It is forbidden to use our platform in any way that poses a risk or could potentially harm minors.
+- **Disclosure of Sensitive Information:** Disclosing personal or sensitive information on our app that could endanger someone's safety or privacy is not allowed.
+- **Inappropriate Content:** Uploading, downloading, distributing, or sharing content that fails to meet our established content guidelines is prohibited.
+- **Unsolicited Communications:** Our platform should not be used for the distribution of unsolicited promotional messages, advertisements, or any form of spam.
+- **Distribution of Malicious Software:** The act of disseminating viruses, worms, Trojan horses, spyware, adware, or any software designed to harm or illicitly access data or personal information is strictly forbidden.
+
+## Purpose of Data Collection and Processing
+
+At edio, we collect and process personal data to provide our educational services, ensure user authentication, enhance platform functionality, and comply with legal obligations. The legal basis for processing your data includes user consent, the performance of a contract, and compliance with legal obligations.
+
+## Data Retention Policy
+
+Personal data will be retained for as long as necessary to fulfil the purposes for which it was collected, including providing services, complying with legal obligations, and resolving disputes. Retention periods are determined based on legal requirements and the nature of the data.
+
+## User Rights
+
+Under GDPR, you have the right to:
+- Access your personal data and obtain a copy.
+- Rectify inaccurate or incomplete data.
+- Erase your personal data under certain conditions.
+- Restrict the processing of your data.
+- Object to the processing of your data.
+- Request data portability.
+
+To exercise these rights, please contact us at [contact@edio.cc](mailto:contact@edio.cc).
+
+## Third-Party Processors
+
+We work with the following third-party processors who may have access to your data:
+- **MongoDB Atlas:** For secure data storage.
+- **Axios:** Provides HTTP client services for making requests to servers.
+
+For more information, please see their respective Privacy Policies. All third-party processors are contractually obligated to comply with GDPR requirements.
+
+## Cookie Policy
+
+edio uses cookies to enhance user experience, analyse app usage, and provide personalised content.
+
+## AI-Enabled Educational Content Guidelines
+
+### AI Content Safeguards
+- **Content Review Processes:** Utilising both automated systems and human oversight to ensure content adheres to our educational standards.
+- **Ongoing Evaluation and Adjustment:** Regular assessment of AI-generated content for compliance with educational and safety standards.
+- **Encouragement of User Feedback:** We invite users to provide feedback on the accuracy and appropriateness of AI-generated content.
+- **Adherence to AI Ethical Use Principles:** Following guidelines for ethical and responsible use of AI in educational content creation.
+
+### Data Privacy and Protection
+- **Commitment to User Privacy:** We are dedicated to protecting user privacy and will not disclose personal data without explicit consent.
+- **Anonymization Techniques:** Employing methods to anonymize user data during query processing to ensure privacy.
+- **Implementation of Robust Security Measures:** Applying industry-standard security protocols to safeguard user data.
+
+### Integrity of Educational Content
+- **Focus on Content Accuracy and Pertinence:** Our aim is to provide content that is both accurate and highly relevant, using credible resources to enhance learning materials.
+- **Dedication to Continual Enhancement:** We commit to regularly refining the accuracy and relevance of our content through continuous AI training and user feedback.
+
+### User Engagement with AI-Generated Content
+- **Responsible Engagement Expected:** Users are encouraged to engage with AI-generated content thoughtfully and to verify information when making important academic decisions.
+
+## Contact Us
+
+Should you have any inquiries or concerns regarding these Terms and Conditions, please reach out to us at [contact@edio.cc](mailto:contact@edio.cc).
+
+## Acceptance of Terms
+
+By registering and engaging with edio, you affirm that you have thoroughly read, understood, and consented to be bound by these Terms and Conditions. We thank you for choosing edio as your educational ally and look forward to supporting you on your path to academic achievement.
+`;
+
+
+const TermsOfService = () =>
+{
+    return (
+        <div className="markdown-container">
+            <ReactMarkdown className="markdown-content">{markdownTermsOfService}</ReactMarkdown>
+        </div>
+    )
+}
+export default TermsOfService;
