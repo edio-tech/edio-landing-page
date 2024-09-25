20d3e3b7a35a681a50b24d47c84a53f1a3bda484
MatthewFoster02 <mattfoster02@outlook.com>
2024-09-25 16:59:52 +0100

    Header disappears on scroll down and reappears on Scroll Up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,45 @@
+import { useEffect, useState } from 'react';
+
 import './Header.css';
 import HomeLink from './HomeLink';
 import Aplications from './Aplications';
 
 const Header = () =>
 {
+    const [showHeader, setShowHeader] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    const controlHeader = () =>
+    {
+        if (typeof window !== 'undefined')
+        {
+            if (window.scrollY > lastScrollY)
+            {
+                // If we are scrolling down, hide the header
+                setShowHeader(false);
+            } else {
+                // If we are scrolling up, show the header
+                setShowHeader(true);
+            }
+            setLastScrollY(window.scrollY); // Update the last scroll position
+        }
+    };
+
+    useEffect(() =>
+    {
+        if (typeof window !== 'undefined')
+        {
+            window.addEventListener("scroll", controlHeader); // Add scroll listener
+
+            return () =>
+            {
+                window.removeEventListener("scroll", controlHeader); // Cleanup listener
+            };
+        }
+    }, [lastScrollY]);
+
     return (
-        <header className="header-container">
+        <header className={`header-container ${showHeader ? 'header-visible' : 'header-hidden'}`}>
             <HomeLink width="2.75rem" fontSize="var(--fs-header)" font_colour="var(--clr-text)" />
             <Aplications />
         </header>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,20 +1,24 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import useHeaderShown from "../hooks/useHeaderShown";
 
 
 const Landing = () =>
 {
     const navigate = useNavigate();
+    const { setHeaderShown } = useHeaderShown();
 
     const checkForAplication = () =>
     {
         const aplication = localStorage.getItem('aplication');
         if(aplication)
         {
+            setHeaderShown(true);
             navigate(`/${aplication}`);
         }
         else
         {
+            setHeaderShown(false);
             navigate('/choose');
         }
     }
