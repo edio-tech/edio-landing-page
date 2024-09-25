45d4c3fed873298ca4f9db82cbed003b089f09a4
MatthewFoster02 <mattfoster02@outlook.com>
2024-09-25 12:04:09 +0100

    Create footer elements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,41 @@
 import './Footer.css';
+import HomeLink from './HomeLink';
 
 const Footer = () =>
 {
     return (
         <div className="footer-container">
-            Footer
+            <div className="links">
+                <div className="info-links">
+                    <div className="vertical-columns">
+                        <div className="connect">
+                            <h3>Connect</h3>
+                            <div className="connect-links">
+                                <a>contact@edio.cc</a>
+                                <a>support@edio.cc</a>
+                            </div>
+                        </div>
+                        <div className="legal">
+                            <h3>Company</h3>
+                            <div className="legal-links">
+                                <a>Terms of Service</a>
+                                <a>Privacy Policy</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="socials">
+                        
+                    </div>
+                </div>
+                <div className="logo-link">
+                    <HomeLink width="5rem" font_size="var(--fs-h1)" font_colour="var(--clr-text-accent)" />
+                </div>
+            </div>
+            <div className="copyright">
+                <p>© 2024</p>
+                <p>All rights reserved</p>
+                <p>We are in beta</p>
+            </div>
         </div>
     )
 }
